745bc34c47070c2c128a9e366e2eee93daf6bcda
Рыбин Данила <dgrybin@localadmins-MacBook-Pro.local>
2025-12-01 15:23:30 +0300

    Second commit

--- src/main/java/ru/practicum/LaterApplication.java
+++ src/main/java/ru/practicum/LaterApplication.java
@@ -4,12 +4,20 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import ru.practicum.config.AppConfig;
 
 public class LaterApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(LaterApplication.class);
+
     public static void main(String[] args) throws LifecycleException {
+
+        log.info("Запуск приложения Later...");
+
         Tomcat tomcat = new Tomcat();
         tomcat.getConnector().setPort(8080);
 
@@ -17,9 +25,17 @@ public class LaterApplication {
 
         AnnotationConfigWebApplicationContext applicationContext =
                 new AnnotationConfigWebApplicationContext();
+
+        // ➜ Добавляем конфигурацию, которая грузит application.properties
+        applicationContext.register(AppConfig.class);
+
+        // ➜ Сканируем компоненты
         applicationContext.scan("ru.practicum");
+
         applicationContext.setServletContext(tomcatContext.getServletContext());
         applicationContext.refresh();
+
+        log.info("Spring контекст успешно загружен");
 
         // добавляем диспетчер запросов
         DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
@@ -27,6 +43,8 @@ public class LaterApplication {
                 Tomcat.addServlet(tomcatContext, "dispatcher", dispatcherServlet);
         dispatcherWrapper.addMapping("/");
         dispatcherWrapper.setLoadOnStartup(1);
+
+        log.info("Tomcat запускается на порту 8080");
 
         tomcat.start();
     }
--- src/main/java/ru/practicum/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/user/UserServiceImpl.java
@@ -1,22 +1,34 @@
 package ru.practicum.user;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService {
+
     private final UserRepository repository;
+
+    private final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Value("${app.welcome-message}")
+    private String welcomeMessage;
 
     @Override
     public List<User> getAllUsers() {
+        log.info("Запрос на получение всех пользователей");
+        log.debug("Welcome message: {}", welcomeMessage);
         return repository.findAll();
     }
 
     @Override
     public User saveUser(User user) {
+        log.info("Сохранение нового пользователя: {}", user);
         return repository.save(user);
     }
 }
